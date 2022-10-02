32d27a28e2fb9772ee455fe48f2ef7feb11fc343
Miguel Tornero <a00820449@itesm.mx>
2022-10-01 20:05:22 -0500

    added redirect

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { createBrowserRouter, RouterProvider, Navigate } from "react-router-dom";
 import Placeholder from "./pages/Placeholder";
 
 const router = createBrowserRouter([{
     path: "/",
     element: <Placeholder/>
+},{
+    path: "*",
+    element: <Navigate to="/" />
 }])
 
 export default function App() {
